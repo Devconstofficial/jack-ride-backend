354e645fe9ef82c7de72f80148e7d92b276b1c84
Ali Hussain <alihussainpid@gmail.com>
2024-10-25 05:55:11 +0500

    end time test

--- sockets/ride.sockets.js
+++ sockets/ride.sockets.js
@@ -81,7 +81,7 @@ io.on('connection', (socket) => {
         rideRequests[socket.owner] = {ownerId: socket.owner, leavingFrom, goingTo, dateForBooking, hours}
         //Get Available Riders
         let startTime = new Date(dateForBooking);
-        let endTime = startTime + hours*3600;
+        let endTime = new Date(startTime + hours*3600);
 
         console.log(startTime, endTime);
 
